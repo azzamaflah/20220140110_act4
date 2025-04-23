27a2a0539d357287db8fc36ac4ad03f87b13b420
azzamaflah <azzamhebat46@gmail.com>
2025-04-23 20:29:50 +0700

    Menambahkan field password dengan validasi pada RegisterPage

--- lib/RegisterPage.dart
+++ lib/RegisterPage.dart
@@ -36,4 +36,15 @@ class _RegisterpageState extends State<Registerpage> {
                   return null;
                 },
               ),
+              TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
 }

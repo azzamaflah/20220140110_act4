b759d435c2839033f47933cc6a41823608bb62c7
azzamaflah <azzamhebat46@gmail.com>
2025-04-23 20:26:09 +0700

    Menambahkan controller untuk email dan password pada RegisterPage

---
+++ lib/RegisterPage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Registerpage extends StatefulWidget {
+  const Registerpage({super.key});
+
+  @override
+  State<Registerpage> createState() => _RegisterpageState();
+
+}
+
+class _RegisterpageState extends State<Registerpage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

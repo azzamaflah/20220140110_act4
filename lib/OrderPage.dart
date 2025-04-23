07afffd9faf35adf8e52ac30d216decf54734d91
azzamaflah <azzamhebat46@gmail.com>
2025-04-23 20:41:24 +0700

    Menambahkan controller untuk input makanan, minuman, dan jumlahnya pada OrderPage

---
+++ lib/OrderPage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Orderpage extends StatefulWidget {
+  const Orderpage({super.key});
+
+  @override
+  State<Orderpage> createState() => _OrderpageState();
+}
+
+class _OrderpageState extends State<Orderpage> {
+  final TextEditingController makananController = TextEditingController();
+  final TextEditingController minumanController = TextEditingController();
+  final TextEditingController jumlahMakananController = TextEditingController();
+  final TextEditingController jumlahMinumanController = TextEditingController();
+  int totalHarga = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
